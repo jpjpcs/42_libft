be4596c476f4da9b570fab204c83a70a49dcfcd7
jpjpcs <joaopaulojesuspereiracosilva@gmail.com>
2023-05-02 12:02:33 +0100

    update strrchr

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jode-jes <jode-jes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 20:02:44 by jode-jes          #+#    #+#             */
-/*   Updated: 2023/04/28 20:34:39 by jode-jes         ###   ########.fr       */
+/*   Updated: 2023/05/02 11:34:28 by jode-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,22 @@ char * ft_strrchr(const char *s, int c)
     int i;
 
     len = strlen(s);
-    i = len - 1;
+    i = (int) len - 1;
     while (i >= 0)
     {
-        if ((&(s[i]) == c)
-            return ((char *)s);
+        if ((s[i]) == c)
+            return ((char *)&s[i]);
         i--;
-    }
-    if (*s == c)
-        return ((char *)s);
-    return (NULL);    
+    } 
+    if (s[i] == c)
+        return ((char *)&s[i]);
+    return (NULL);  
 }
 
-int main()
+/* int main()
 {
-    char str[] = "hello world";
+    char str[] = "hello word";
     char *ptr2 = strrchr(str, 'l');
     printf("ptr2 points to %c\n", *ptr2); 
     return (0);
-}
+} */
