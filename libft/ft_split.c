df9507ca44561e40223101ef504e9e9c38c8cacf
Joao Silva <joaopaulojesuspereiracosilva@gmail.com>
2023-05-06 12:23:28 +0100

    split incomplete

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jode-jes <jode-jes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 19:22:57 by jode-jes          #+#    #+#             */
-/*   Updated: 2023/05/06 02:32:19 by jode-jes         ###   ########.fr       */
+/*   Updated: 2023/05/06 12:22:20 by jode-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,72 @@
 
 size_t ft_count_words(char const *s, char c)
 {   
-    size_t i;
+    size_t c;
     size_t n_of_words;
 
-    i = 0;
+    c = 0;
     n_of_words = 0;
-    while (s[i] && s[i] == c)
-        i++;
-    while (s[i])
+    while (s[c] && s[c] == c)
+        c++;
+    while (s[c])
     {
-        if (s[i] != c)
+        if (s[c] != c)
             n_of_words++;
-        while ((s[i] && s[i] != c)
-            i++;
-        while ((s[i] && s[i] == c);
-            i++; 
+        while ((s[c] && s[c] != c)
+            c++;
+        while ((s[c] && s[c] == c);
+            c++; 
     }
     return(n_of_words);
 }
 
+size_t ft_count_letters(char const *s, char c, size_t w)
+{
+    size_t n_of_words;
+    size_t c;
+    size_t n_of_letters;
+    
+    n_of_words = 0;
+    c = 0;
+    while (s[c] && s[c] == c)
+        c++;
+    while (s[c]) && n_of_words != w)
+    {
+        if s[c] != c)
+            n_of_words++;
+        while (s[c] && s[c] != c)
+            c++;
+        while (s[c] && s[c] == c)
+            c++;
+    }
+    //n_of_words = 0; obsoleto?
+    n_of_letters = 0;
+    while (s[c] && s[c] != c)
+    {
+        c++;
+        n_of_letters ++;  
+    }
+    return(n_of_letters);
+}
 
 char **ft_split(char const *s, char c)
 {
     char **new_array;
     size_t n_of_words;
+    size_t w;
     
     n_of_words = (ft_count_words(s, c));
-    new_array = (char*)malloc(sizeof(char *) * (n_of_words + 1));  
+    new_array = (char*)malloc(sizeof(char *) * (n_of_words + 1));
+    if (!new_array)
+        return (NULL);
+    w = 0;
+    while(n_of_words > w)
+    {   
+        new_array[j] = (char *)malloc(sizeof(char))* (ft_count_letters(s, c, w) + 1))
+        n_of_words--;
+    }
+
+    // i = 0 obsoleto?
     
 }
 
@@ -51,3 +90,7 @@ int main()
     printf("new list: %s",ft_split(str,c));
     return (0);
 }
+
+//c goes through each character (letter or space or other) in the loop of ft_count_words. 
+//In ft_split, w goes through each word. Its a counter that compares the
+//n_of_words (that returns of ft_n_of_words) and the w that starts in zero.
