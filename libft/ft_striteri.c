d8b01646cae7aeeced3c9efe3923ac1efe5d06f1
Joao Silva <joaopaulojesuspereiracosilva@gmail.com>
2023-05-08 22:37:27 +0100

    striteri completed

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -1,47 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jode-jes <jode-jes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/08 18:50:57 by jode-jes          #+#    #+#             */
-/*   Updated: 2023/05/08 21:04:39 by jode-jes         ###   ########.fr       */
+/*   Created: 2023/05/08 21:05:30 by jode-jes          #+#    #+#             */
+/*   Updated: 2023/05/08 22:10:12 by jode-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	char	*new_string;
-	int		i;
-	int		len;
+	unsigned int		i;
 
 	i = 0;
-	len = ft_strlen(s);
-	new_string = (char *)malloc(sizeof(char) * (len + 1));
-	if (!new_string)
-		return (NULL);
 	while (s[i])
 	{
-		new_string[i] = f(i, s[i]);
+		f(i, &s[i]);
 		i++;
 	}
-	new_string[i] = '\0';
-	return (new_string);
 }
 
-/* char ft_add_index (unsigned int i, char c)
+/* void ft_add_index (unsigned int i, char *c)
 {
-    return (c + i);
+    *c += i;
 }
 
 int	main(void)
 {
     char str[] = "Hello";
-    char *modified_str = ft_strmapi (str, ft_add_index);
-    printf("\nModified str is: %s\n\n", modified_str);
-    free(modified_str);
+    printf("\nOriginal str is: %s\n\n", str);
+    ft_striteri (str, ft_add_index);
+    printf("\nModified str is: %s\n\n\n", str);
     return (0);
 } */
